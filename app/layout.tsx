4c65829a2ed3b5aea3cce1c57469d14a3e6963fe
YehorStepanov <stepegorr@gmail.com>
2026-01-30 21:17:58 +0100

    create header

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import 'modern-normalize';
 import "./globals.css";
 import Header from "@/components/Header/Header";
 import { comfortaa, lato } from "./fonts";
--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,7 +1,43 @@
+'use client';
+
+import Link from 'next/link';
+import css from './Header.module.css';
+import { usePathname } from 'next/navigation';
+import clsx from 'clsx';
+
 export default function Header() {
+  const pathname = usePathname();
+
   return (
-    <header>
-      <h1>My App Header</h1>
+    <header className={css.header}>
+      <nav className={css.nav}>
+        <Link className={css.logo_link} href="/">
+          <svg className={css.logo} width="136" height="15">
+            <use href="/img/sprite.svg#icon-TravelTrucks"></use>
+          </svg>
+        </Link>
+        <ul className={css.nav_links}>
+          <li className={css.nav_item}>
+            <Link
+              className={clsx(
+                css.nav_item,
+                pathname.startsWith('/') && css.active,
+              )}
+              href="/"
+            >
+              Home
+            </Link>
+          </li>
+          <li className={css.nav_item}>
+            <Link className={clsx(
+                css.nav_item,
+                pathname.startsWith('/catalog') && css.active,
+              )} href="/catalog">
+              Catalog
+            </Link>
+          </li>
+        </ul>
+      </nav>
     </header>
   );
 }
